07d59a31ccb1561b1ac727be48e456b004258c65
Patrice LONLA <splonla@gmail.com>
2026-08-31 21:06:44 +0100

    Fixed issue affecting app building. Now it builds but couldn't yet install it either on device or emulator.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,25 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+// Certains plugins Flutter (ex. another_telephony) ciblent encore la JVM 1.8, ce qui
+// entre en conflit avec la cible Java d'AGP. On aligne tout le monde sur la 17.
+// À déclarer avant le bloc `evaluationDependsOn` ci-dessous, sinon `:app` est déjà
+// évalué quand on tente d'enregistrer le `afterEvaluate`.
+subprojects {
+    afterEvaluate {
+        extensions
+            .findByType(com.android.build.api.dsl.CommonExtension::class.java)
+            ?.compileOptions
+            ?.apply {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
--- lib/parsing/sms_listener.dart
+++ lib/parsing/sms_listener.dart
@@ -1,4 +1,4 @@
-import 'package:telephony/telephony.dart';
+import 'package:another_telephony/telephony.dart';
 
 import '../db/app_database.dart';
 import '../parsing/sms_parser.dart';
